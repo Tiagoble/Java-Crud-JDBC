5f7f9b00b160afddeab83921209845c6e69c09e9
Tiago <ti.abreu.da.rocha@gmail.com>
2025-09-26 20:06:57 -0300

    connecting to the database

---
+++ demo/src/main/java/edu/tiago/crud/util/ConnectionDB.java
@@ -0,0 +1,34 @@
+package edu.tiago.crud.util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionDB {
+    private static Connection connection = null;
+    private Properties properties = loadProperties();
+    private String url = properties.getProperty("url");
+
+    public Connection getConnection(){
+        if(connection == null){
+            try{
+                connection = DriverManager.getConnection(url, properties);
+            }catch (SQLException e){
+                throw new RuntimeException("Error connecting to the database", e);
+            }
+        }
+        return connection;
+    }
+
+    private Properties loadProperties() {
+        Properties props = new Properties();
+        try {
+            props.load(getClass().getClassLoader().getResourceAsStream("db.properties"));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load database properties", e);
+        }
+        return props;
+    }
+}
